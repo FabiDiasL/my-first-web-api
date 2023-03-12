494e3ef532effa24c8d024684099cf5f4c0a35e6
FabiDiasL <fabi_lima_@hotmail.com>
2023-03-11 22:32:49 -0300

    update

--- src/main/java/DIO/myfirstwebapi/controller/WelcomeController.java
+++ src/main/java/DIO/myfirstwebapi/controller/WelcomeController.java
@@ -1,9 +1,11 @@
 package DIO.myfirstwebapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class WelcomeController {
 	
 	@GetMapping
